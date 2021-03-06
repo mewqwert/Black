29135655ad0664f8d01f37ea50359d4785dd789f
22p21c0286 <55741073+mewqwert@users.noreply.github.com>
2021-03-06 14:22:15 +0700

    Update Black.ino

--- Black/Black.ino
+++ Black/Black.ino
@@ -6,10 +6,14 @@
 
 Encoder myEnc_L(2, 12); // Encoder of Motor left
 Encoder myEnc_R(3, 13); // Encoder of Motor Right
+const int PPR = 1881; // pulse per rotation of motor
 long oldPosition_L  = -999;
 long oldPosition_R  = -999;
 long newPosition_L;
 long newPosition_R;
+long Pulse_L = 0;
+long Pulse_R = 0;
+long Time_start = micros();
 
 ros::NodeHandle nh;
 
@@ -45,19 +49,32 @@ void setup() {
   nh.subscribe(Motor_R);
   nh.advertise(EncL);
   nh.advertise(EncR);
+  V_L_msg.data = 0;
+  V_R_msg.data = 0;
 } 
 
 void loop() {
   newPosition_L = myEnc_L.read();
   newPosition_R = myEnc_R.read()*-1;
   
+  
   if (newPosition_L != oldPosition_L) {
     oldPosition_L = newPosition_L;
     encL_msg.data = newPosition_L;
+    Pulse_L ++;
     }
   if (newPosition_R != oldPosition_R) {
     oldPosition_R = newPosition_R;
     encR_msg.data = newPosition_R;
+    Pulse_R ++;
+    }
+  float dt = (float)((micros() - Time_start)/1000000);
+  if (dt > 1) {
+    V_L_msg.data = (Pulse_L/PPR)*60/dt;
+    V_R_msg.data = (Pulse_R/PPR)*60/dt;
+    Pulse_L = 0;
+    Pulse_R = 0;
+    Time_start = micros();
     }
   if((abs(newPosition_L)>1000000)||(abs(newPosition_R)>1000000)) {
     myEnc_L.write(0);
